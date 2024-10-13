00b466d2383a31884d4a83de0e4fe9b496665183
Midomik <krastikrabbs9234@gmail.com>
2024-10-13 21:48:18 +0300

    feat: implements routing and pagination

--- src/app/data/services/car.service.ts
+++ src/app/data/services/car.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Car } from '../intefaces/car.interface';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +13,26 @@ export class CarService {
 
   constructor() {}
 
-  getCars() {
-    return this.http.get<Car[]>(this.baseUrl + 'cars');
+  getCars(page: number = 1, limit: number = 12) {
+    const params = { page, limit };
+    return this.http.get<Car[]>(this.baseUrl + 'cars', { params });
+  }
+
+  getCarsWithAddressParts(page: number = 1, limit: number = 12) {
+    return this.getCars(page, limit).pipe(
+      map((cars) => {
+        return cars.map((car) => {
+          const addressParts = car.address.split(', ');
+          return {
+            ...car,
+            address: {
+              street: addressParts[0],
+              city: addressParts[1],
+              country: addressParts[2],
+            },
+          };
+        });
+      })
+    );
   }
 }
